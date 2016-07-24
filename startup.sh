19192436cfe2ceb8bf01a5881d4175bc3c8b5a76
Björn Grüning <bjoern@gruenings.eu>
2016-07-24 20:45:08 +0200

    Adopt startup file to latest changes

--- startup.sh
+++ startup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-if [ "$dataset_hid" != "" ]; then
-  pvpython /usr/local/lib/paraview-5.1/site-packages/paraview/web/pv_web_visualizer.py \
-         --content /usr/local/share/paraview-5.1/www \
-         --data-dir /import \
-         --save-data-dir /export \
-	 --load-file $dataset_hid \
-         --port 8777
+if [ -z "$DATASET_HID" ]; then
+    pvpython /usr/local/lib/paraview-5.1/site-packages/paraview/web/pv_web_visualizer.py \
+        --content /usr/local/share/paraview-5.1/www \
+        --data-dir /input \
+        --save-data-dir /export \
+        --port 8777 \
+        --host 0.0.0.0 \
+        -d
 else
-  pvpython /usr/local/lib/paraview-5.1/site-packages/paraview/web/pv_web_visualizer.py \
-         --content /usr/local/share/paraview-5.1/www \
-         --data-dir /import \
-         --save-data-dir /export \
-         --port 8777
+    pvpython /usr/local/lib/paraview-5.1/site-packages/paraview/web/pv_web_visualizer.py \
+        --content /usr/local/share/paraview-5.1/www \
+        --data-dir /input \
+        --save-data-dir /export \
+        --load-file input.tiff \
+        --port 8777 \
+        --host 0.0.0.0 \
+        -d
 fi

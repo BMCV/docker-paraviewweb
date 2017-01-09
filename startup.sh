f34fe17508820f3277fb0fee41d9d2eb3e495eec
Thomas Wollmann <thomas.wollmann@bioquant.uni-heidelberg.de>
2017-01-09 11:17:13 +0100

    changed startup app

--- startup.sh
+++ startup.sh
@@ -4,18 +4,14 @@ service nginx start
 
 if [ -z "$DATASET_HID" ]
 then
-    pvpython /usr/local/lib/node_modules/pvw-visualizer/server/pvw-visualizer.py \
-	--paraview $CONDA/lib/paraview-5.2/
+    Visualizer --paraview $CONDA/lib/paraview-5.2/ \
         --data /input \
         --port 8777 \
 	--server-only
 else
-    pvpython /usr/local/lib/node_modules/pvw-visualizer/server/pvw-visualizer.py \
-	--paraview $CONDA/lib/paraview-5.2/
+    Visualizer --paraview $CONDA/lib/paraview-5.2/ \
         --data /input \
         --port 8777 \
 	--server-only \
         --load-file $DATASET_HID
 fi
-
-libvtkPVServerManagerRendering-pv5.2.so

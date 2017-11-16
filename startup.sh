05f741d9f3ba3b57a3e0d21df4869a232b289cc2
ThomasWollmann <tom.wollmann@ish.de>
2017-11-16 11:41:58 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,13 +4,11 @@ service nginx start
 
 if [ -z "$DATASET_HID" ]
 then
-    Visualizer --paraview $CONDA/lib/paraview-5.2/ \
-        --data /input \
+    Visualizer --data /input \
         --port 8777 \
 	--server-only
 else
-    Visualizer --paraview $CONDA/lib/paraview-5.2/ \
-        --data /input \
+    Visualizer --data /input \
         --port 8777 \
 	--server-only \
         --load-file $DATASET_HID
